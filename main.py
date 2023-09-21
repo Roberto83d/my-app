cbf7081713b49fb3fe3ff5c60f753bfa31ae61a9
Roberto83d <143013856+Roberto83d@users.noreply.github.com>
2023-09-21 17:37:35 +0200

    nowa aplikacja
    kalkulator

---
+++ main.py
@@ -0,0 +1,545 @@
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w styczniu")
+
+inflation1 = 1.5
+rata_po_inflacji = loan - rata - inflation1
+print(rata_po_inflacji)
+
+print("twoja rata po inflacji to")
+print(loan - rata - inflation1 - loan)
+print(" ,,,,,,,,,,  ")
+
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w lutym")
+
+inflation2 = -0.4
+rata_po_inflacji = loan - rata - inflation2
+# rata_po_inflacji -> loan - rata - infaltion2
+print(rata_po_inflacji)
+
+print("twoja rata po inflacji to")
+print(loan - rata - inflation2 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((rata_po_inflacji - loan) - (loan - rata - inflation1 - loan))
+print(",,,,,,,,,, ")
+
+
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w marcu")
+
+inflation3 = 2.3
+print(loan - rata - inflation3)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation3 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation2 - loan) - (loan  - rata - inflation3 - loan))
+print("  ,,,,,,,,,,, ")
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w kwietniu")
+
+inflation4 = 1.2
+print(loan - rata - inflation4)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation4 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation3 - loan) - (loan  - rata - inflation4 - loan))
+print("  ,,,,,,,,,,, ")
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w maju")
+
+inflation5 = 1.7
+print(loan - rata - inflation5)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation5 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation4 - loan) - (loan  - rata - inflation5 - loan))
+print("  ,,,,,,,,,,, ")
+
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w czerwiec")
+
+inflation6 = 2.3
+print(loan - rata - inflation6)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation6 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation5 - loan) - (loan  - rata - inflation6 - loan))
+print("  ,,,,,,,,,,, ")
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w lipcu")
+
+inflation7 = 1.5
+print(loan - rata - inflation7)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation7 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation6 - loan) - (loan  - rata - inflation7 - loan))
+print("  ,,,,,,,,,,, ")
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w sierpniu")
+
+inflation8 = 1.7
+print(loan - rata - inflation8)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation8 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation7 - loan) - (loan  - rata - inflation8 - loan))
+print("  ,,,,,,,,,,, ")
+
+
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w wrzesniu")
+
+inflation9 = 2.3
+print(loan - rata - inflation9)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation9 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation8 - loan) - (loan  - rata - inflation9 - loan))
+print("  ,,,,,,,,,,, ")
+
+
+
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w pazdzierniuku")
+
+inflation10 = 0.6
+print(loan - rata - inflation10)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation10 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation9 - loan) - (loan  - rata - inflation10 - loan))
+print("  ,,,,,,,,,,, ")
+
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w listopadzie")
+
+inflation11 = 2.3
+print(loan - rata - inflation11)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation11 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation10 - loan) - (loan  - rata - inflation11 - loan))
+print("  ,,,,,,,,,,, ")
+
+
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w grudniu")
+
+inflation12 = 0.3
+print(loan - rata - inflation12)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation12 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation11 - loan) - (loan  - rata - inflation12 - loan))
+print("        ")
+print("       ")
+
+
+print(" nowy rok rozliczeniowy ")
+print("        ")
+print("    I    ")
+print("    I    ")
+print("    I    ")
+print("    I    ")
+print(" IIIIIII   ")
+print("  IIIII    ")
+print("   III  ")
+print("    I  ")
+
+print("        ")
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w styczniu")
+
+inflation1 = 2.3
+rata_po_inflacji = loan - rata - inflation1
+print(rata_po_inflacji)
+
+print("twoja rata po inflacji to")
+print(loan - rata - inflation1 - loan)
+print(" ,,,,,,,,,,  ")
+
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w lutym")
+
+inflation2 = -0.3
+rata_po_inflacji = loan - rata - inflation2
+# rata_po_inflacji -> loan - rata - infaltion2
+print(rata_po_inflacji)
+
+print("twoja rata po inflacji to")
+print(loan - rata - inflation2 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((rata_po_inflacji - loan) - (loan - rata - inflation1 - loan))
+print(",,,,,,,,,, ")
+
+
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w marcu")
+
+inflation3 = 1.5
+print(loan - rata - inflation3)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation3 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation2 - loan) - (loan  - rata - inflation3 - loan))
+print("  ,,,,,,,,,,, ")
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w kwietniu")
+
+inflation4 = 0.2
+print(loan - rata - inflation4)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation4 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation3 - loan) - (loan  - rata - inflation4 - loan))
+print("  ,,,,,,,,,,, ")
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w maju")
+
+inflation5 = 2.4
+print(loan - rata - inflation5)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation5 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation4 - loan) - (loan  - rata - inflation5 - loan))
+print("  ,,,,,,,,,,, ")
+
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w czerwiec")
+
+inflation6 = 0.2
+print(loan - rata - inflation6)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation6 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation5 - loan) - (loan  - rata - inflation6 - loan))
+print("  ,,,,,,,,,,, ")
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w lipcu")
+
+inflation7 = 1.4
+print(loan - rata - inflation7)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation7 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation6 - loan) - (loan  - rata - inflation7 - loan))
+print("  ,,,,,,,,,,, ")
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w sierpniu")
+
+inflation8 = 1
+print(loan - rata - inflation8)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation8 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation7 - loan) - (loan  - rata - inflation8 - loan))
+print("  ,,,,,,,,,,, ")
+
+
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w wrzesniu")
+
+inflation9 = 1.4
+print(loan - rata - inflation9)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation9 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation8 - loan) - (loan  - rata - inflation9 - loan))
+print("  ,,,,,,,,,,, ")
+
+
+
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w pazdzierniuku")
+
+inflation10 = 1.5
+print(loan - rata - inflation10)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation10 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation9 - loan) - (loan  - rata - inflation10 - loan))
+print("  ,,,,,,,,,,, ")
+
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w listopadzie")
+
+inflation11 = 0
+print(loan - rata - inflation11)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation11 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation10 - loan) - (loan  - rata - inflation11 - loan))
+print("  ,,,,,,,,,,, ")
+
+
+
+print("calkowita kwota kredytu")
+
+loan = 12000
+print(loan)
+
+print("twoja stala rata to")
+rata = 200
+print(rata)
+
+print("pozostala kwota do splaty w grudniu")
+
+inflation12 = 1.1
+print(loan - rata - inflation12)
+
+print("twoja rata po inflacji to")
+print(loan  - rata - inflation12 - loan)
+
+print("ruznica w kwocie z poprzedniego miesiaca wynosi")
+print((loan  - rata - inflation11 - loan) - (loan  - rata - inflation12 - loan))
+
+
+
+
